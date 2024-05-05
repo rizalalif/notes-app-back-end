3d3469b720ec6dcccc0c86424d00cd150a76e7e2
rizalalif <rizalalif03@gmail.com>
2024-05-06 03:51:31 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const handler = require("./handler");
 const init = async () => {
   const server = await Hapi.server({
     port: 5000,
-    host: "localhost",
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ["*"],
